b637ca977f90435f8a88bfb7b04f69a70b4e94a2
Aouatef Djeghri <awatef.djeghri5@gmail.com>
2020-12-11 23:18:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+function Header(props) {
+  return (
+    <header>
+      <h1>{props.name}'s Kitchen</h1>
+    </header>
+  );
+}
+
+function Main() {
+  return (
+    <main>
+      <h3>We serve delicius food </h3> 
+    </main>
+  );
+}
+
+function Footer(props) {
+  return (
+    <footer>
+      <h4>{props.year}</h4>
+    </footer>
+  );
+}
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Awatef" />
+      <Main/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
